019c9c67deed48e12b329340335644c62f09d5de
Gabriel Bernardo <gabriel.bernardo@computacao.ufla.br>
2019-10-24 19:41:01 -0300

    Menu NavBar alterado

--- Code/index.php
+++ Code/index.php
@@ -1,33 +1,21 @@
 <?php
 include_once 'includes/header.php';
 ?>
-<div class="nav-wrapper deep-purple darken-2">
-<nav class="center nav-wrapper deep-purple darken-2"> <h3>Kaiba Corporation</h3></nav>
-<a href="#" class="sidenav-trigger" data-target="menu-side">
-  <i class="material-icons">menu</i>
-  </a>
 
-<ul class="sidenav" id="menu-side">
-<li><div class="user-view">
-      <div class="background">
-        <img src="includes/adds/YU.jpg" width=300 height=170>
-      </div>
-      <a href="#user"><img class="circle"   src="includes/adds/SK.jpg"></a>
-      <a href="#name"><span class="white-text name">Seto Kaiba</span></a>
-      <a href="#email"><span class="white-text email">kaiba@coporation.com</span></a>
-
-    </div></li>
-    <li><a href="login/perfil.php"><i class="material-icons">face</i>Perfil</a></li>
-    <li><a href="adicionar.php"><i class="material-icons">edit</i>Adicionar card</a></li>
+<div class="nabar-fixed">
+<nav>
+<div class="nav-wrapper  indigo darken-3">
+<a href="index.php" class="brand-logo center">Kaiba Corporation</a>
+<ul id="nav-mobile" class="right hide-on-med-and-down" >
+    <li><a href="login/perfil.php"><i class="material-icons">person</i></a></li>
+    <li><a href="adicionar.php"><i class="material-icons">edit</i></a></li>
     <li><div class="divider"></div></li>
-  </ul>
-  
-  
+  </ul>  
   </li>
 </ul>
+
+</nav>
 </div>
-
-
 <div class="row">
   <div class="col s12 m6 push-m3 ">
   <h3 class="light">Cards</h3>
@@ -63,7 +51,7 @@ include_once 'includes/header.php';
         <td>Brabo</td>
 
         <td> <a href=""class="btn-floating green"> <i class="material-icons ">edit</i></a> </td>  
-        <td> <a href=""class="btn-floating red"><i class="material-icons ">create</i></a> </td>  
+        <td> <a href=""class="btn-floating red"><i class="material-icons ">delete</i></a> </td>  
 
       </tr>
     </tbody>
